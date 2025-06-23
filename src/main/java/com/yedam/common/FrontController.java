2e427feeec2ea8fe508ba716b3eebdfa60209f18
gywns <gywns8339@naver.com>
2025-06-23 17:54:54 +0900

    0623

---
+++ src/main/java/com/yedam/common/FrontController.java
@@ -0,0 +1,38 @@
+package com.yedam.common;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.yedam.service.MainControl;
+
+
+// 서블릿
+public class FrontController extends HttpServlet {
+	Map<String, Control> map;
+	
+	public FrontController() {
+		map = new HashMap<String, Control>();
+	}
+	
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		map.put("/main.do", new MainControl()); // 글 목록
+	}
+	
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// url이 호출(http://localhots:8080/Project66/main.do) -> 페이지 호출 -> Control
+		String uri = req.getRequestURI(); // /Project66/main.do
+		String context = req.getContextPath(); // /Project66
+		String page = uri.substring(context.length()); // /main.do
+		Control sub = map.get(page); // .do 이름이 키 값이기 때문에 밸류인 Control 형태의 변수에 저장
+		sub.exec(req, resp); // 각 Control에 있는 exec 메소드 호출
+	}
+}
---
+++ src/main/webapp/WEB-INF/jsp/main.jsp
@@ -0,0 +1,7 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+    <div class="container-fluid py-5 mb-5 hero-header">
+            <div class="container py-5">
+                <div class="row g-5 align-items-center">
+<H3>메인</H3>
+</div></div></div>
