1444ad69362cd0551f74d0dc4ebd478890b21a57
Your Name <you@example.com>
2025-06-30 17:02:20 +0900

    어려운 페이지네이션

--- src/main/webapp/js/product/productPage.js
+++ src/main/webapp/js/product/productPage.js
@@ -70,7 +70,7 @@ function pageEvent(){
 			paginationAll(page);
 			svc.productList(page);
 			} else {
-				paginationSearch(page);
+				
 				svc.searchList(page); 
 			}
 			
@@ -78,50 +78,6 @@ function pageEvent(){
 	})
 }
 
-function paginationSearch(page) {
-	document.querySelector("#search2").addEventListener('input', async function(e) {
-	let keyword = e.target.value;
-	let data = await fetch("productSearchPage.do?keyword=" + keyword)
-	let result = await data.json();
-			let totalCnt = result.totalCnt;
-			let start, end;
-			let prev, next;
-			let realEnd = Math.ceil(totalCnt / 9);
-			end = Math.ceil(page / 10) * 10;
-			start = end - 9;
-			end = end > realEnd ? realEnd : end;
-			prev = start > 1;
-			next = end < realEnd;
-
-			let target = document.querySelector('.pagination');
-			target.innerHTML = "";
-
-			let str = "";
-			if (prev) {
-				str = `<a href="#" class="rounded" data-page="${start - 1}">&laquo;</a>`;
-			} else {
-				str = `<a href="#" class="rounded disabled">&laquo;</a>`;
-			}
-			target.insertAdjacentHTML('beforeend', str);
-
-			for (let p = start; p <= end; p++) {
-				if (p == page) {
-					str = `<a href="#" class="active rounded" data-page="${p}">${p}</a>`;
-				} else {
-					str = `<a href="#" class="rounded" data-page="${p}">${p}</a>`;
-				}
-				target.insertAdjacentHTML('beforeend', str);
-			}
-
-			if (next) {
-				str = `<a href="#" class="rounded" data-page="${end + 1}">&raquo;</a>`;
-			} else {
-				str = `<a href="#" class="rounded disabled">&raquo;</a>`;
-			}
-			target.insertAdjacentHTML('beforeend', str);
-			})
-	
-}
 
 
 
